3f0e1d2c4394d19d061338a6acb4cfcdd1321522
Alexey Kachura <alexonthework@gmail.com>
2023-10-14 12:14:53 +0200

    Add Fastlane

---
+++ Calculator/Calculator2 AssigmentTests/Calculator2_AssigmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator2_AssigmentTests.swift
+//  Calculator2 AssigmentTests
+//
+//  Created by Alexey Kachura on 16.10.23.
+//
+
+import XCTest
+
+final class Calculator2_AssigmentTests: XCTestCase {
+    
+    private var value = 0
+    
+    func testExample() {
+        XCTAssertEqual(value, 0)
+    }
+}
---
+++ Calculator/CalculatorUITests/CalculatorUITests.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorUITests.swift
+//  CalculatorUITests
+//
+//  Created by Alexey Kachura on 15.10.23.
+//
+
+import XCTest
+
+final class CalculatorUITests: XCTestCase {
+    func testExample() throws {
+        
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+        app.buttons["7"].tap()
+        snapshot("01test")
+
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
